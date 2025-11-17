e6f9cfe133aef593b0a696fcfaae0613eb5cb8f7
Al Mubin <almubin5060@gmail.com>
2025-11-17 15:31:18 +0000

    fix Console Error
    Hydration failed because the server rendered HTML did

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -7,17 +7,15 @@ const schema = z.object({
   code: z.string().trim().min(1, { message: 'Code is required.' }),
 });
 
-export async function joinChat(formData: FormData) {
+export async function joinChat(prevState: { error: any } | undefined, formData: FormData) {
   const parsed = schema.safeParse({
     code: formData.get('code'),
   });
 
   if (!parsed.success) {
-    // In a real app, you would handle this error more gracefully, e.g., by returning an error message.
-    // For this example, we will simply not redirect on failure.
     return {
       error: parsed.error.flatten().fieldErrors,
-    }
+    };
   }
 
   redirect(`/chat/${parsed.data.code}`);
