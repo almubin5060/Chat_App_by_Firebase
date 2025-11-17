fe4ff37512b43b1d12f123964f46a992381f9b17
Al Mubin <almubin5060@gmail.com>
2025-11-17 16:37:00 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,3 +1,10 @@
 
 // This configuration is replaced by the App Hosting plugin during compilation.
-export const firebaseConfig = {};
+export const firebaseConfig = {
+  "projectId": "studio-4010619228-e69e4",
+  "appId": "1:110346234128:web:9251430aea15af8b6c5d95",
+  "apiKey": "AIzaSyBKT2r0yEN3Uv8puoaqKgfuC8IXA0MuRMo",
+  "authDomain": "studio-4010619228-e69e4.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "110346234128"
+};
